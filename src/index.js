682f684ed4a7c356e36506a254cc3bff78a605cf
Shane D Wilson <me@shanedwilson.com>
2018-11-14 19:23:12 -0600

    auth working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+import firebase from 'firebase/app';
+import 'bootstrap';
+
+import apiKeys from '../db/apiKeys.json';
+
+import auth from './components/Auth/auth';
+
 import './index.scss';
 
-console.log('index.js');
+const initApp = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  auth.loginBtn();
+};
+
+initApp();
