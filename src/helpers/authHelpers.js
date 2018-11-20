cc8ecba07214eaf63129ee201515a4841eace0f3
Shane D Wilson <me@shanedwilson.com>
2018-11-20 08:59:57 -0600

    added trash and pencil icons

--- src/helpers/authHelpers.js
+++ src/helpers/authHelpers.js
@@ -6,10 +6,12 @@ const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       $('#auth').hide();
+      $('#tasks').show();
       $('#navbar-button-auth').hide();
       $('#navbar-button-logout').show();
     } else {
       $('#auth').show();
+      $('#tasks').hide();
       $('#navbar-button-auth').show();
       $('#navbar-button-logout').hide();
     }
